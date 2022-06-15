a0f9f750a72467e10ed0a6d81eaf4ba5889b9d9d
Uno-Takashi <euno.eng@gmail.com>
2022-06-15 05:35:51 +0000

    init

---
+++ tests/test_ShieldsData.py
@@ -0,0 +1,10 @@
+from django_dynamic_shields.data import ShieldsData
+
+
+class TestCreateShieldsData:
+    def test__create_ok_data(self):
+        """Simplest normal case"""
+        sd:ShieldsData = ShieldsData(label="label", message="message")
+        sd_dict:dict=sd.dict
+        assert sd_dict["label"]=="label"
+        assert "namedLogo" not in sd_dict
