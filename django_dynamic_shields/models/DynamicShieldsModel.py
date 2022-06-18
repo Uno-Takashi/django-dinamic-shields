00299bbd042865d8cc54890d9a553da15de9667a
Takashi Uno <euno.eng@gmail.com>
2022-06-18 11:29:11 +0900

    init

--- django_dynamic_shields/models/DynamicShieldsModel.py
+++ django_dynamic_shields/models/DynamicShieldsModel.py
@@ -1,17 +1,17 @@
-from django.db.models import Model
+from django.db.models import Model, CharField, IntegerField, BooleanField, DateTimeField
 
 
 class ShieldsDataModel(Model):
-    label = models.CharField(max_length=50)
-    message = models.CharField(max_length=50)
-    color = models.CharField(max_length=15, default="lightgrey")
-    labelColor = models.CharField(max_length=15, default="grey")
-    isError = models.BooleanField(default=False)
-    namedLogo = models.CharField(max_length=30, null=True)
-    logoSvg = models.CharField(max_length=50000, null=True)
-    logoColor = models.CharField(max_length=15, null=True)
-    logoWidth = models.CharField(max_length=15, null=True)
-    logoPosition = models.CharField(max_length=15, null=True)
-    style = models.CharField(max_length=15, default="flat")
-    cacheSeconds = models.IntegerField(default=300)
-    updated_datetime = models.DateTimeField(auto_now=True)
+    label = CharField(max_length=50)
+    message = CharField(max_length=50)
+    color = CharField(max_length=15, default="lightgrey")
+    labelColor = CharField(max_length=15, default="grey")
+    isError = BooleanField(default=False)
+    namedLogo = CharField(max_length=30, null=True)
+    logoSvg = CharField(max_length=50000, null=True)
+    logoColor = CharField(max_length=15, null=True)
+    logoWidth = CharField(max_length=15, null=True)
+    logoPosition = CharField(max_length=15, null=True)
+    style = CharField(max_length=15, default="flat")
+    cacheSeconds = IntegerField(default=300)
+    updated_datetime = DateTimeField(auto_now=True)
