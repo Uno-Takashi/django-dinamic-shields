36f396e930a62981ab7343194cd9a7b33d61fc30
Takashi Uno <euno.eng@gmail.com>
2022-06-18 12:13:38 +0900

    init

--- django_dynamic_shields/models/DynamicShieldsModel.py
+++ django_dynamic_shields/models/DynamicShieldsModel.py
@@ -1,4 +1,5 @@
 from django.db.models import Model, CharField, IntegerField, BooleanField, DateTimeField
+from django_dynamic_shields.data import ShieldsData
 
 
 class ShieldsDataModel(Model):
@@ -15,3 +16,19 @@ class ShieldsDataModel(Model):
     style = CharField(max_length=15, default="flat")
     cacheSeconds = IntegerField(default=300)
     updated_datetime = DateTimeField(auto_now=True)
+
+    def get_shields_data(self) -> ShieldsData:
+        return ShieldsData(
+            label=self.label,
+            message=self.message,
+            color=self.color,
+            labelColor=self.labelColor,
+            isError=self.isError,
+            namedLogo=self.namedLogo,
+            logoSvg=self.logoSvg,
+            logoColor=self.logoColor,
+            logoWidth=self.logoWidth,
+            logoPosition=self.logoPosition,
+            style=self.style,
+            cacheSeconds=self.cacheSeconds,
+        )
